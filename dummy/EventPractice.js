695e8922cd1b709c61a5243f33ed7a53d27e7d05
zero2755 <56134028+zero2755@users.noreply.github.com>
2022-03-28 01:38:26 +0900

    Update EventPractice.js

--- dummy/EventPractice.js
+++ dummy/EventPractice.js
@@ -3,11 +3,24 @@ import React, { Component } from "react";
 const alertGood = () => {
   alert("good");
 };
-
+//화살표함수로 따로 빼놓는게 가독성좋음(바벨이용한다함, 생성자에 일일히 bind안해도됨)
 class EventPractice extends Component {
   state = {
     message: "default",
   };
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      message: e.target.value,
+    });
+  };
+  handleInit = () => {
+    this.setState({
+      message: "버튼으로초기화됨",
+    });
+  };
+
   render() {
     return (
       <div>
@@ -18,12 +31,9 @@ class EventPractice extends Component {
           name="message"
           value={this.state.message}
           placeholder="아무거나 입력해 보세요"
-          onChange={(e) => {
-            //console.log(e.target.value);
-            this.setState({ message: e.target.value });
-            console.log(e.target.value);
-          }}
+          onChange={this.handleChange}
         />
+        <button onClick={this.handleInit}>메시지 초기화하기</button>
       </div>
     );
   }
